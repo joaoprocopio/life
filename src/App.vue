f48c331bc82efbb5c29fb17b5babf97d48c74efa
joaoprocopio <joaovitorcprocopio@gmail.com>
2022-12-26 15:12:05 -0300

    remove coisarada inutil

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <main>hello world!</main>
 </template>
 
 <style scoped>
