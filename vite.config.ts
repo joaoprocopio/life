2a1a09dba390bb9607addfe6cf8ad56507ec3a1e
joaoprocopio <joaovitorcprocopio@gmail.com>
2023-05-04 00:51:22 -0300

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 3000
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: "/life"
 })
